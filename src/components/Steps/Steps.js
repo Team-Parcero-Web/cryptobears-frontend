391aebcc2750acbdf39eddd8f9caa6586094d5e2
Oscar Cordero <ocordero@applaudostudios.com>
2021-03-28 02:20:30 -0600

    ✨  Added steps section

--- src/components/Steps/Steps.js
+++ src/components/Steps/Steps.js
@@ -1,17 +1,30 @@
 import React from "react";
-import { CenteredContent } from "../lib";
+import { CenteredContent, Heading } from "../lib";
 import styled from "styled-components";
 import StepRow from "../StepRow/StepRow";
+import steps from "../../data/steps";
 
 const Steps = () => {
   return (
     <CenteredContent>
-      <Inner>
-        <StepRow />
-      </Inner>
+      <StepsGrid>
+        <Heading>How to get a bear?</Heading>
+        {steps.map(
+          (step) =>
+            console.log(step.id % 2 == 0) || (
+              <StepRow step={step} textPosition={step.id % 2 == 0 ? "right" : "left"} />
+            )
+        )}
+      </StepsGrid>
     </CenteredContent>
   );
 };
 
-const Inner = styled.div``;
+const StepsGrid = styled.div`
+  text-align: center;
+  padding: 0 2rem;
+  display: grid;
+  width: 100%;
+  padding-top: 10rem;
+`;
 export default Steps;
--- src/data/steps.js
+++ src/data/steps.js
@@ -1,21 +1,23 @@
-export const steps = [
+const steps = [
   {
-    step:
-      "Download and install the plugin called MetaMask in your Chrome browser. This allowed websites (only that you authorize) to access your Binance Smart Chain account safely.",
-    image: "/image.png",
+    id: 1,
+    stepText:
+      "Download and install the plugin called MetaMask in your Chrome browser. This will allow websites (only that you authorize) to access your Binance Smart Chain account safely.",
+    image: "/images/step1.jpeg",
+  },
+  { id: 2, stepText: "Add the binance smart chain configuration.", image: "/images/step2.jpeg" },
+  { id: 3, stepText: "If you made a new account, buy some BNB.", image: "/images/step3.jpeg" },
+  {
+    id: 4,
+    stepText: "On the BNB withdrawal window, choose the BSC chain.",
+    image: "/images/step4.jpeg",
   },
   {
-    step: "Add the binance smart chain configuration.",
-    image: "/image.png",
-  },
-  {
-    step:
-      " Download and install the plugin called MetaMask in your Chrome browser. This allowed websites (only that you authorize) to access your Binance Smart Chain account safely.",
-    image: "/image.png",
-  },
-  {
-    step:
-      " Download and install the plugin called MetaMask in your Chrome browser. This allowed websites (only that you authorize) to access your Binance Smart Chain account safely.",
-    image: "/image.png",
+    id: 5,
+    stepText:
+      "Once you have completed these steps, you'll be able to login on our site and befriend a random bear!",
+    image: "/images/step1.jpeg",
   },
 ];
+
+export default steps;
